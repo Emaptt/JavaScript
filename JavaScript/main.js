afb35d0c09dc6e45bf92f455a17f09c851eba6f1
Emanuel Panza <emanuelgpanza@gmail.com>
2023-01-24 04:37:31 -0300

    Segunda pre-entrega proyecto final

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,44 +1,80 @@
-let tiempoEspera = 0;
-let turnoNumero = 0;
-let espera = 0;
-
-function alertaNumero() {
-    turnoNumero++;
-    alert ("Su numero es " + turnoNumero + " y su tiempo de espera es de " + espera + " minutos.");
-    espera = espera + tiempoEspera;
+class Pax {
+    constructor (nombre, apellido, edad, dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+    } 
 }
 
+function ingresarPax(){
 
-let tipoConsulta = prompt ("Ingrese su tipo de consulta: Ventas, Atencion o Caja. Sino escriba Cerrar").toLowerCase();
+    for (let i=1; i<= cantidadPax; i++){
+        let nombre = prompt ("Ingrese el nombre de la persona " + i );
+        let apellido = prompt ("Ingrese el apellido de la persona " + i);
+        let edad = prompt ("Ingrese la edad de la persona " + i);
+        let dni = prompt ("Ingrese el número de documento de la persona " + i);
+    
+        personas.push (new Pax (nombre, apellido, edad, dni));
+    }
+}
 
-while( tipoConsulta !== "cerrar"){ 
+function informacionArrayPax(){
+    
+    personas.forEach ( (persona) => {
+        alert (`Los datos ingresados son:
+    Nombre: ${persona.nombre} ${persona.apellido} 
+    Edad: ${persona.edad}
+    DNI: ${persona.dni}`)
+    })
+};
 
-    switch(tipoConsulta){
 
-        case "ventas":
-            tiempoEspera = 15;
-            alertaNumero();
-            break;
+const personas = []
 
-        case "atencion":
-            tiempoEspera = 10;
-            alertaNumero();
-            break;
+let cantidadPax = parseInt (prompt ("Ingrese con números la cantidad de personas que viajan"));
 
-        case "caja":
-            tiempoEspera = 5;
-            alertaNumero();
+ingresarPax();
+
+informacionArrayPax();
+
+let mensaje = prompt ("Si desea modificar los datos de alguna persona escriba volver, sino escriba continuar").toLowerCase();
+
+while (mensaje !== "continuar"){
+
+    switch (mensaje){
+
+        case "volver":
+            const personaAModificar = personas [(parseInt (prompt ("Ingrese el numero de la persona a modificar"))) - 1];
+
+            if (personas.indexOf (personaAModificar) == -1){
+
+                alert ("El numero de la persona no se encuentra en la lista");
+
+            } else {
+
+                personas.splice (personas.indexOf (personaAModificar), 1 );
+
+                cantidadPax = parseInt (prompt ("Ingrese con números la cantidad de personas que quiere agregar"));
+
+                ingresarPax();
+
+                informacionArrayPax()
+
+            }
+
             break;
 
         default:
-            alert ("Usted ingreso un tipo de consulta invalido, por favor intente nuevamente")
+            alert ("Los datos ingresados son incorrectos, por favor intente nuevamente");
             break;
-
     }
 
-    tipoConsulta = prompt("Ingrese su tipo de consulta: Ventas, Atencion o Caja. Sino escriba Cerrar").toLowerCase();
-
+    mensaje = prompt ("Si desea modificar los datos de alguna persona escriba volver, sino escriba continuar").toLowerCase();
 }
 
-alert ("Hay " + turnoNumero + " turnos en total y el tiempo de espera es de " + espera + " minutos.");
 
+let cantidadHabitaciones = parseInt (prompt (`Ingrese en numeros la cantidad de habitaciones que desea reservar. 
+Puede ser entre ${Math.round(personas.length / 2)} o ${personas.length} ya que cada una tiene un cupo de 2 personas.`));
+
+alert (`Usted reservó ${cantidadHabitaciones} habitaciones para ${personas.length} personas`)
